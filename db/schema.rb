bafbf1e2c154a32c4f1380ed55d81ead73bc4b92
Ciaran Edwards <ciaran.edwards@live.com>
2017-04-24 10:01:01 +0100

    The db migration wasn't running because the slots were being created before the events. I changed the timestamp of the file so the slots would run after the events migration to fix the issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423151432) do
+ActiveRecord::Schema.define(version: 20170423151511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20170423151432) do
     t.integer  "duration_minutes"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.index ["event_id"], name: "index_slots_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_slots_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,6 @@ ActiveRecord::Schema.define(version: 20170423151432) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "slots", "events"
+  add_foreign_key "slots", "users"
 end
