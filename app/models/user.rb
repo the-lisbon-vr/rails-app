8b403d708bb0cecc20171bcccb83218e505e1522
Carlos Silva <cucmaluco@gmail.com>
2017-05-19 12:48:47 +0100

    confirmable working

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,8 @@ class User < ApplicationRecord
 
   private
 
-  def send_welcome_email
-    UserMailer.welcome(self).deliver_now
-  end
+  # def send_welcome_email
+  #   UserMailer.welcome(self).deliver_now
+  # end
 
 end
